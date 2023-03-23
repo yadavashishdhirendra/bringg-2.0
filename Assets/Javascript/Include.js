07aa35d83033c623384fe4eb670a8ff3834366b4
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2023-03-23 12:35:46 +0530

    solutions pages

---
+++ Assets/Javascript/Include.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#header").load(
+        "Assets/Include/header.html"
+    );
+});
+
+$(document).ready(function () {
+    $("#footers").load(
+        "Assets/Include/footer.html"
+    );
+});
