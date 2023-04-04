8b1b57de4f133c2571284d283fef6eeec67c252b
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2023-04-04 16:16:05 +0530

    added logo

--- Assets/Javascript/Include.js
+++ Assets/Javascript/Include.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    $("#header").load(
-        "Assets/Include/header.html"
-    );
-});
-
-$(document).ready(function () {
     $("#footerss").load(
         "Assets/Include/footer.html"
     );
