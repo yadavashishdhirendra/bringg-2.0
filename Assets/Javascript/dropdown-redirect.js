2654484e5c22bf226a51a6df992be966c6258b42
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2023-04-06 12:19:40 +0530

    updated all pages

--- Assets/Javascript/dropdown-redirect.js
+++ Assets/Javascript/dropdown-redirect.js
@@ -1,3 +1,7 @@
 document.getElementById("about").onclick = function () {
     location.href = "https://yadavashishdhirendra.github.io/bringg-2.0/about-us.html";
 };
+
+document.getElementById("about_mobile").onclick = function () {
+    location.href = "https://yadavashishdhirendra.github.io/bringg-2.0/about-us.html";
+};
